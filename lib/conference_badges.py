12c488fe65ea3f8c37039eac4f48f82f1895ce8d
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-01 15:57:03 -0400

    complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,17 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    badge_batch = [f'Hello, my name is {name}.' for name in names]
+    return badge_batch
 
 def assign_rooms(names):
-    return None
+    room_batch = [f"Hello, {name}! You'll be assigned to room {index}!" for index, name in enumerate(names, 1)]
+    return room_batch
 
 def printer(names):
+    for text in batch_badge_creator(names):
+        print(text)
+    for text in assign_rooms(names):
+        print(text)
     return None
